a91d9fa5390a4e3dcf833ce880246075da2410be
Davi <davicamiloribeiro@gmail.com>
2022-03-27 15:20:49 -0300

    Atualizar e deletar itens

--- src/controllers/apiController.ts
+++ src/controllers/apiController.ts
@@ -41,3 +41,25 @@ export const getPhrase = async (req: Request, res: Response) => {
         res.json({ error: 'Frase não encontrada' })
     }
 }
+
+export const updatePhrase = async (req: Request, res: Response) => {
+    let { id } = req.params;
+    let { author, txt } = req.body;
+
+    let phrase = await Phrase.findByPk(id);
+    if(phrase){
+        phrase.author = author;
+        phrase.txt = txt;
+        await phrase.save();
+
+        res.json({ phrase });
+    }else{
+        res.json({ error: 'Frase não encontrada' })
+    }
+}
+
+export const deletePhrase = async (req: Request, res: Response) => {
+    let { id } = req.params;
+    await Phrase.destroy({ where: { id } });
+    res.json({});
+}
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -11,5 +11,7 @@ router.get('/nome/:nome', ApiController.nome);
 router.post('/frases', ApiController.createPhrase);
 router.get('/frases', ApiController.listPhrases);
 router.get('/frases/:id', ApiController.getPhrase);
+router.put('/frases/:id', ApiController.updatePhrase);
+router.delete('/frases/:id', ApiController.deletePhrase);
 
 export default router;
